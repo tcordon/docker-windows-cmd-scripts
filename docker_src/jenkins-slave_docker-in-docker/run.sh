2bbbe031bbfda3b35693390f03d8a4b7ac862676
Toni <toni.cordon@gmail.com>
2019-05-07 10:22:06 +0200

    Move jenkins imges to igm_docker repository

--- docker_src/jenkins-slave_docker-in-docker/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -i --name jenkins-jnlp-slave --init --restart always -v agent1-workdir:/home/jenkins/agent -v /var/run/docker.sock:/var/run/docker.sock  jenkins/jnlp-slave -url http://192.168.223.231:8080/ -workDir=/home/jenkins/agent e9342ddddfc400f4d7ab93de0f15ba3ef015e5925f4a889e7ed5ce20e59a5b35 DockerIGMSrv02
